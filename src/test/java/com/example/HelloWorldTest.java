80176478ecc4f9850ab10367a0d029e7f1b20685
TEEYUCHEN <teeyuchen1013@1utar.my>
2025-04-21 16:24:20 +0800

    new

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -2,12 +2,10 @@ package com.example;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 public class HelloWorldTest {
-
     @Test
     public void testGetGreeting() {
-        Main main = new Main();  // Use the correct class name: "Main"
-        assertEquals("Hello, World!", main.getGreeting());
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals("Hello, World!", helloWorld.getGreeting());
     }
 }
